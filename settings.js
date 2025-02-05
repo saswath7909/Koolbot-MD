75f753e53c84e8971a149acdb1dac247d390be29
saswath7909 <145915950+saswath7909@users.noreply.github.com>
2025-02-05 10:54:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Kool',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Kool Bot",
+  botOwner: 'SaswathKool', // Your name
+  ownerNumber: '917858892834', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
